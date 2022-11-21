59ddd23534043134d4eecb4d23cef0922a241426
Taylor P <taylorpahl97@gmail.com>
2022-11-21 12:49:29 -0600

    Added date & time in header, first time block

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Work-Day-Scheduler
+
+
+
+## Credits
+
+https://stackoverflow.com/questions/18536726/javascript-to-display-the-current-date-and-time
+
+https://stackoverflow.com/questions/59995703/trying-to-change-background-color-based-off-if-the-hour-is-in-the-past-current
---
+++ script.js
@@ -0,0 +1,43 @@
+// Displays date and time in header
+
+// get a new date (locale machine date time)
+var date = new Date();
+// get the date as a string
+var n = date.toDateString();
+// get the time as a string
+var time = date.toLocaleTimeString();
+
+// find the html element with the id of currentDay
+// set the innerHTML of that element to the date a space the time
+document.getElementById('currentDay').innerHTML = n + ' ' + time;
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+
+// Colors time blocks based on past / present / future
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
